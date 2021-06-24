1f5fcb6922fdcb82c784a012d274f66afc328fc7
S R <simonray@hotmail.com>
2021-06-24 18:23:20 +0100

    Initial commit

---
+++ ConsoleTvMaze/ConsoleTvMaze/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleTvMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
