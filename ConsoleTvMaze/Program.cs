3292cfb963f55abd1001b6581a447d8b6ff61511
S R <simonray@hotmail.com>
2021-11-25 17:31:50 +0000

    Upgrade to .NET 6

--- ConsoleTvMaze/Program.cs
+++ ConsoleTvMaze/Program.cs
@@ -1,31 +1,21 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿
 using Microsoft.Extensions.Configuration;
 
-namespace ConsoleTvMaze
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine("ConsoleTvMaze");
+Console.WriteLine("ConsoleTvMaze");
 
-            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-            var builder = new ConfigurationBuilder()
-                .AddJsonFile($"appsettings.json", true, true)
-                .AddJsonFile($"appsettings.{env}.json", true, true)
-                .AddEnvironmentVariables();
+var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+var builder = new ConfigurationBuilder()
+    .AddJsonFile($"appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{env}.json", true, true)
+    .AddEnvironmentVariables();
 
-            var config = builder.Build();
+var config = builder.Build();
 
-            AppSettings settings = new AppSettings();
-            config.GetSection("AppSettings").Bind(settings);
+AppSettings settings = new AppSettings();
+config.GetSection("AppSettings").Bind(settings);
 
-            var services = new TvmazeService();
-            await services.Go(settings.TvIncludes);
+var services = new TvmazeService();
+await services.Go(settings.TvIncludes);
 
-            Console.Write("Press <any> key to continue");
-            Console.ReadKey();
-        }
-    }
-}
+Console.Write("Press <any> key to continue");
+Console.ReadKey();
